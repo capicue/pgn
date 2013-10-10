4bd9ffa3e39877e3be3a8ce20bb92f651b7485df
Stacey Touset <stacey@touset.org>
2013-10-10 15:47:35 -0700

    Make Latin-1 encoding optional

--- lib/pgn.rb
+++ lib/pgn.rb
@@ -12,8 +12,13 @@ module PGN
   # @param pgn [String] a pgn representation of one or more chess games
   # @return [Array<PGN::Game>] a list of games
   #
-  def self.parse(pgn)
-    pgn.force_encoding(Encoding::ISO_8859_1)
+  # @note The PGN spec specifies Latin-1 as the encoding for PGN files, so
+  #   this is default.
+  #
+  # @see http://www.chessclub.com/help/PGN-spec PGN Specification
+  #
+  def self.parse(pgn, encoding = Encoding::ISO_8859_1)
+    pgn.force_encoding(encoding) if encoding
 
     PGN::Parser.new.parse(pgn).map do |game|
       PGN::Game.new(game[:moves], game[:tags], game[:result])
