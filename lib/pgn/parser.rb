ed08a0dcbc254bbc86d19315250edff7428d4a73
Stacey Touset <capicue@gmail.com>
2017-02-27 11:54:50 -0800

    Cleanup parser and parser_spec

--- lib/pgn/parser.rb
+++ lib/pgn/parser.rb
@@ -5,46 +5,48 @@ module PGN
   # context free grammar.
   #
   class Parser < Whittle::Parser
-    rule(:wsp => /\s+/).skip!
+    rule(wsp: /\s+/).skip!
 
-    rule("[")
-    rule("]")
-    rule("(")
-    rule(")")
+    rule('[')
+    rule(']')
+    rule('(')
+    rule(')')
 
     start(:pgn_database)
 
     rule(:pgn_database) do |r|
       r[].as { [] }
-      r[:pgn_game, :pgn_database].as {|game, database| database << game }
+      r[:pgn_game, :pgn_database].as { |game, database| database << game }
     end
 
     rule(:pgn_game) do |r|
-      r[:tag_section, :movetext_section].as {|tags, moves| {tags: tags, result: moves.pop, moves: moves} }
+      r[:tag_section, :movetext_section].as do |tags, moves|
+        { tags: tags, result: moves.pop, moves: moves }
+      end
     end
 
     rule(:tag_section) do |r|
-      r[:tag_pair, :tag_section].as {|pair, section| section.merge(pair) }
+      r[:tag_pair, :tag_section].as { |pair, section| section.merge(pair) }
       r[:tag_pair]
     end
 
     rule(:tag_pair) do |r|
-      r["[", :tag_name, :tag_value, "]"].as {|_, a, b, _| {a => b} }
+      r['[', :tag_name, :tag_value, ']'].as { |_, a, b, _| { a => b } }
     end
 
     rule(:tag_value) do |r|
-      r[:string].as {|value| value[1...-1] }
+      r[:string].as { |value| value[1...-1] }
     end
 
     rule(:movetext_section) do |r|
-      r[:element_sequence, :game_termination].as {|a, b| a.reverse << b }
+      r[:element_sequence, :game_termination].as { |a, b| a.reverse << b }
     end
 
     rule(:element_sequence) do |r|
-      r[:element, :element_sequence].as {|element, sequence| element.nil? ? sequence : sequence << element }
+      r[:element, :element_sequence].as do |element, sequence|
+        element.nil? ? sequence : sequence << element
+      end
       r[].as { [] }
-      #r[:recursive_variation, :element_sequence]
-      #r[:recursive_variation]
     end
 
     rule(:element) do |r|
@@ -56,35 +58,36 @@ module PGN
       end
       r[:comment].as { nil }
     end
-    
+
     rule(:san_move_annotated) do |r|
       r[:san_move].as { |move| MoveText.new(move) }
-      r[:san_move, :comment].as { |move, comment| MoveText.new(move, nil, comment) }
-      r[:san_move, :numeric_annotation_glyph].as { |move, annotation| MoveText.new(move, annotation) }
-      r[:san_move, :numeric_annotation_glyph, :comment].as { |move, annotation, comment| MoveText.new(move, annotation, comment) }
-      r[:san_move, :comment, :numeric_annotation_glyph].as { |move, comment, annotation| MoveText.new(move, annotation, comment) }
+      r[:san_move, :comment].as do |move, comment|
+        MoveText.new(move, nil, comment)
+      end
+      r[:san_move, :numeric_annotation_glyph].as do |move, annotation|
+        MoveText.new(move, annotation)
+      end
+      r[:san_move, :numeric_annotation_glyph, :comment].as do |move, annotation, comment|
+        MoveText.new(move, annotation, comment)
+      end
+      r[:san_move, :comment, :numeric_annotation_glyph].as do |move, comment, annotation|
+        MoveText.new(move, annotation, comment)
+      end
     end
 
     rule(:variation_list) do |r|
-        r[:variation, :variation_list].as { |variation, sequence| sequence << variation }
-        r[:variation].as { |v| [v] }
+      r[:variation, :variation_list].as do |variation, sequence|
+        sequence << variation
+      end
+      r[:variation].as { |v| [v] }
     end
-    
+
     rule(:variation) do |r|
-      r["(", :element_sequence, ")"].as { |_, sequence, _| sequence }
+      r['(', :element_sequence, ')'].as { |_, sequence, _| sequence }
     end
-
-#     rule(:variation_sequence) do |r|
-#       r[:element, :variation_sequence]
-#       r[].as { [] }
-#     end
-
-    #rule(:recursive_variation) do |r|
-      #r["(", :element_sequence, ")"]
-    #end
 
     rule(
-      :string => %r{
+      string: /
         "                          # beginning of string
         (
           [[:print:]&&[^\\"]] |    # printing characters except quote and backslash
@@ -92,11 +95,11 @@ module PGN
           \\"                      # escaped quotation marks
         )*                         # zero or more of the above
         "                          # end of string
-      }x
+      /x
     )
 
     rule(
-      :comment => %r{
+      comment: /
         (
           \{                           # beginning of comment
           (
@@ -109,11 +112,11 @@ module PGN
           )*                           # zero or more of the above
           \}                           # end of comment
         )
-      }x
+      /x
     )
 
     rule(
-      :game_termination => %r{
+      game_termination: %r{
         1-0       |    # white wins
         0-1       |    # black wins
         1\/2-1\/2 |    # draw
@@ -122,13 +125,13 @@ module PGN
     )
 
     rule(
-      :move_number_indication => %r{
+      move_number_indication: /
         [[:digit:]]+\.*    # one or more digits followed by zero or more periods
-      }x
+      /x
     )
 
     rule(
-      :san_move => %r{
+      san_move: %r{
         (
           --                           |    # "don't care" move (used in variations)
           [O0](-[O0]){1,2}             |    # castling (O-O, O-O-O)
@@ -149,16 +152,16 @@ module PGN
     )
 
     rule(
-      :tag_name => %r{
+      tag_name: /
         [A-Za-z0-9_]+    # letters, digits and underscores only
-      }x
+      /x
     )
 
     rule(
-      :numeric_annotation_glyph => %r{
+      numeric_annotation_glyph: /
         \$\d+       | # dollar sign followed by an integer from 0 to 255
         [\?!][\?!]?   # support the most used annotations directly
-      }x
+      /x
     )
   end
 end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,84 +1,70 @@
 require 'spec_helper'
 
 describe PGN do
-  describe "parsing a file" do
-    it "should return a list of games" do
-      games = PGN.parse(File.read("./examples/immortal_game.pgn"))
+  describe '.parse' do
+    it 'should return a list of games' do
+      games = PGN.parse(File.read('./examples/immortal_game.pgn'))
       games.length.should == 1
       game = games.first
-      game.result.should == "1-0"
-      game.tags["White"].should == "Adolf Anderssen"
-      game.moves.last.should == "Be7#"
+      game.result.should == '1-0'
+      game.tags['White'].should == 'Adolf Anderssen'
+      game.moves.last.should == 'Be7#'
     end
-  end
 
-  context "alternate castling notation" do
-    describe "parsing a file" do
-      it "should return a list of games" do
-        games = PGN.parse(File.read("./spec/pgn_files/alternate_castling.pgn"))
-        game = games.first
-        game.tags["White"].should == "Somebody"
-        game.result.should == "*"
-        game.moves.last.should == "O-O-O"
-      end
+    it 'should handle alternate castling notation' do
+      games = PGN.parse(File.read('./spec/pgn_files/alternate_castling.pgn'))
+      game = games.first
+      game.tags['White'].should == 'Somebody'
+      game.result.should == '*'
+      game.moves.last.should == 'O-O-O'
     end
-  end
 
-  context "annotations" do
-    describe "parsing a file" do
-      it "should return a list of games" do
-        games = PGN.parse(File.read("./spec/pgn_files/annotations.pgn"))
-        games.each do |game|
-          game.tags["White"].should == "Fool"
-          game.result.should == "0-1"
-          game.moves.last.should == "Qh4#"
-        end
+    it 'should handle annotations' do
+      games = PGN.parse(File.read('./spec/pgn_files/annotations.pgn'))
+      games.each do |game|
+        game.tags['White'].should == 'Fool'
+        game.result.should == '0-1'
+        game.moves.last.should == 'Qh4#'
       end
     end
 
-    describe "parsing a file" do
-      it "should work with comments" do
-        games = PGN.parse(File.read("./spec/pgn_files/comments.pgn"))
-        game = games.first
-        game.tags["White"].should == "Scholar"
-        game.result.should == "1-0"
-        game.moves.last.should == "Qxf7#"
-      end
-
-      it "should work with multiline comments" do
-        games = PGN.parse(File.read("./spec/pgn_files/multiline_comments.pgn"))
-        game = games.first
-        game.tags["White"].should == "Scholar"
-        game.result.should == "1-0"
-        game.moves.last.should == "Qxf7#"
-      end
+    it 'should handle comments' do
+      games = PGN.parse(File.read('./spec/pgn_files/comments.pgn'))
+      game = games.first
+      game.tags['White'].should == 'Scholar'
+      game.result.should == '1-0'
+      game.moves.last.should == 'Qxf7#'
     end
 
-    describe "parsing a file" do
-      it "should return a list of games" do
-        games = PGN.parse(File.read("./spec/pgn_files/variations.pgn"))
-        game = games.first
-        game.tags["Black"].should == "Petrov"
-        game.result.should == "*"
-        game.moves.last.should == "Nf6"
-      end
+    it 'should handle multiline comments' do
+      games = PGN.parse(File.read('./spec/pgn_files/multiline_comments.pgn'))
+      game = games.first
+      game.tags['White'].should == 'Scholar'
+      game.result.should == '1-0'
+      game.moves.last.should == 'Qxf7#'
     end
-    
-    describe "parsing a complex file" do
-      it "should return a list of games" do
-        games = PGN.parse(File.read("./spec/pgn_files/test.pgn"))
-        game = games.first
-        game.tags["Black"].should == "Gelfand, Boris"
-        game.result.should == "1-0"
-        game.moves[13].should == "Nfd7"
-        game.moves[34].should == "f3"
-        game.moves[35].annotation.should == "$6"
-        game.moves[35].comment.should == "{Gelfand\ndecide tomar medidas.}"
-        game.moves[35].variations.size.should == 1
-        variation = game.moves[35].variations[0]
-        variation.size.should == 2
-        variation[0].should == "Nxf3"
-      end
+
+    it 'should handle variations' do
+      games = PGN.parse(File.read('./spec/pgn_files/variations.pgn'))
+      game = games.first
+      game.tags['Black'].should == 'Petrov'
+      game.result.should == '*'
+      game.moves.last.should == 'Nf6'
+    end
+
+    it 'should handle complex files' do
+      games = PGN.parse(File.read('./spec/pgn_files/test.pgn'))
+      game = games.first
+      game.tags['Black'].should == 'Gelfand, Boris'
+      game.result.should == '1-0'
+      game.moves[13].should == 'Nfd7'
+      game.moves[34].should == 'f3'
+      game.moves[35].annotation.should == '$6'
+      game.moves[35].comment.should == "{Gelfand\ndecide tomar medidas.}"
+      game.moves[35].variations.size.should == 1
+      variation = game.moves[35].variations[0]
+      variation.size.should == 2
+      variation[0].should == 'Nxf3'
     end
   end
 end
